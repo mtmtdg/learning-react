5df5ab397f27408d2e18c68da9ddb79fde70c55c
mtmtdg <mtmtdg@yahoo.com>
2022-10-19 20:22:12 +0900

    using generate-react-cli

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # learning-react
+
+### generate-react-cli
+
+```shell
+npx generate-react-cli c[omponent] Box --type=page
+# or
+yarn generate-react c Table --type=page
+```
+
---
+++ generator-templates/component/TemplateName.tsx
@@ -0,0 +1,12 @@
+import styles from "./TemplateName.module.css";
+
+interface TemplateNameProps {}
+
+export default function TemplateName(props: TemplateNameProps) {
+  return (
+    <div className={styles.TemplateName}>
+      TemplateName works!
+      <div></div>
+    </div>
+  );
+}
