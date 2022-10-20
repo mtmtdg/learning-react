55c64226fdcc5ccaf3b96e3b510602c833058579
mtmtdg <mtmtdg@yahoo.com>
2022-10-20 15:00:56 +0900

    add getcurrentpage and gotopage

--- src/components/Viewer/Viewer.tsx
+++ src/components/Viewer/Viewer.tsx
@@ -1,4 +1,8 @@
-import WebViewer from '@pdftron/webviewer';
+/* non free library pdftron
+   https://www.pdftron.com/api/web/
+   https://www.pdftron.com/api/web/global.html#WebViewerOptions
+ */
+import WebViewer, { WebViewerInstance } from '@pdftron/webviewer';
 import { useEffect, useRef } from 'react';
 
 import styles from './Viewer.module.css';
@@ -6,21 +10,70 @@ import styles from './Viewer.module.css';
 interface ViewerProps {}
 
 export default function Viewer(props: ViewerProps) {
-  const viewer = useRef(null);
+  const viewerElementRef = useRef(null);
+  const viewerInstanceRef = useRef<WebViewerInstance>();
 
   useEffect(() => {
     WebViewer(
       {
         path: '/webviewer/lib',
         initialDoc: 'page4.pptx',
+        disabledElements: ['header'],
+        isReadOnly: true,
+        enableAnnotations: false,
+        forceClientSideInit: true,
+        disableLogs: true,
       },
-      viewer.current!
+      viewerElementRef.current!
     ).then(instance => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+      viewerInstanceRef.current = instance;
 
-      documentViewer.addEventListener('documentLoaded', () => {});
+      const { documentViewer, annotationManager, Tools } = instance.Core;
+      const UI = instance.UI;
+
+      documentViewer.enableReadOnlyMode();
+      annotationManager.enableReadOnlyMode();
+
+      // only show one page
+      UI.setLayoutMode(UI.LayoutMode.Single);
+
+      // disable pan and select button
+      UI.disableTools([Tools.ToolNames.PAN, Tools.ToolNames.EDIT, Tools.ToolNames.MARQUEE]);
+
+      // disable more buttons like [search, comments]
+      UI.disableFeatures([
+        UI.Feature.Search, // search
+        UI.Feature.NotesPanel, // comments
+        UI.Feature.PageNavigation, // mouse scroll
+        UI.Feature.Ribbons, // [View Annotate Shapes Insert EditText FillAndSign Forms]
+        UI.Feature.TextSelection,
+        UI.Feature.Download,
+        UI.Feature.Print,
+        UI.Feature.MultiTab,
+        UI.Feature.MultiViewerMode,
+      ]);
+
+      // hide buttons on header or hide header itself
+      UI.disableElements(['menuButton', 'leftPanelButton', 'viewControlsButton', 'zoomOverlayButton']);
+      UI.disableElements(['header']);
     });
   }, []);
 
-  return <div className={styles.webviewer} ref={viewer}></div>;
+  const getCurrentPage = () => {
+    const documentViewer = viewerInstanceRef.current?.Core.documentViewer;
+    documentViewer && console.log(documentViewer.getCurrentPage());
+  };
+
+  const goToPage3 = () => {
+    const documentViewer = viewerInstanceRef.current?.Core.documentViewer;
+    documentViewer?.setCurrentPage(3, false);
+  };
+
+  return (
+    <>
+      <div className={styles.webviewer} ref={viewerElementRef}></div>
+      <button onClick={getCurrentPage}>get current page</button>
+      <button onClick={goToPage3}>go to 3</button>
+    </>
+  );
 }
