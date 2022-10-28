ebcd928718a8f0e7779c6dd0468b4c5d16079a18
mtmtdg <mtmtdg@yahoo.com>
2022-10-28 23:13:19 +0900

    test full width of parent

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,26 @@
-import styles from "./Home.module.css";
+import { ArrowBack } from '@mui/icons-material';
+import { AppBar, Chip, Container, CssBaseline, IconButton, Stack, Toolbar, Typography } from '@mui/material';
+import styles from './Home.module.css';
 
 interface HomeProps {}
 
 export default function Home(props: HomeProps) {
   return (
     <div className={styles.Home}>
-      Home works!
-      <div></div>
+      <AppBar>
+        <Toolbar>
+          <IconButton>
+            <ArrowBack />
+          </IconButton>
+
+          <Container sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Stack direction="row" alignItems="center" spacing={2}>
+              <Chip label="test"></Chip>
+              <Typography variant="h6">title</Typography>
+            </Stack>
+          </Container>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
