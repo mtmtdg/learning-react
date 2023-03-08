07cfecece723937e1184580e0f7e45d6a726806d
mtmtdg <mtmtdg@yahoo.com>
2023-03-08 16:47:41 +0900

    upgrade home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,11 @@
-import styles from "./Home.module.css";
+import styles from './Home.module.css';
 
 interface HomeProps {}
 
 export default function Home(props: HomeProps) {
   return (
     <div className={styles.Home}>
-      Home works!
-      <div></div>
+      <div>Home works!</div>
     </div>
   );
 }
