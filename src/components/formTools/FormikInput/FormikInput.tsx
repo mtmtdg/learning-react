6d2d9f2db840c80ec3215c35f13e3837d631d7ef
mtmtdg <mtmtdg@yahoo.com>
2023-05-06 17:57:37 +0900

    无法解决optional number的问题

--- src/components/formTools/FormikInput/FormikInput.tsx
+++ src/components/formTools/FormikInput/FormikInput.tsx
@@ -1,12 +1,22 @@
 import { TextField } from '@mui/material';
 import { useField } from 'formik';
 import { CommonFormProps } from '../CommonFormProps';
+import { HTMLInputTypeAttribute } from 'react';
+import { number } from 'zod';
 
-type FormikInputProps = CommonFormProps & {};
+type FormikInputProps = CommonFormProps & { type?: HTMLInputTypeAttribute };
 
-export default function FormikInput({ label, name, required = false, disabled = false }: FormikInputProps) {
-  const [field, state] = useField({ name });
+export default function FormikInput({
+  label,
+  name,
+  type = 'text',
+  required = false,
+  disabled = false,
+}: FormikInputProps) {
+  const [field, state, { setValue }] = useField({ name });
   const { error, touched } = state;
 
-  return <TextField label={label} error={touched && !!error} helperText={touched ? error : ''} {...field} />;
+  return (
+    <TextField type={type} label={label} error={touched && !!error} helperText={touched ? error : ''} {...field} />
+  );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,16 +10,35 @@ import { Add, Delete } from '@mui/icons-material';
 
 interface HomeProps {}
 
+/**
+ * 神奇的是zop面对一个空字符串,竟然不报错,一个optional既允许undefined,也允许''
+ */
 const schema = z.object({
   firstName: z.string().optional(),
   lastName: z.string().optional(),
+  age: z.number().optional(),
   test: z.string().array(),
   arr: z.object({ a: z.string(), b: z.string() }).array(),
 });
 
+/**
+ * 垃圾react认为不能设置值为null,
+ * 还不推荐设置undefined(认为是非受控),
+ * 最终不得不为一个optional number的字段,设置上初始值''
+ * react对此有一些讨论,但不了了之
+ * https://github.com/facebook/react/issues/7779
+ */
+
+/**
+ * formik对此有讨论,但无能力
+ * https://github.com/jaredpalmer/formik/issues/1869
+ * react hook from有讨论,最终似乎是用setValueAs来做转换
+ * https://github.com/orgs/react-hook-form/discussions/6980
+ */
 const initialValues = {
   firstName: '',
   lastName: '',
+  age: '',
   email: '',
   test: [],
   arr: [
@@ -29,6 +48,8 @@ const initialValues = {
 };
 
 const onSubmit = (values: any, { setSubmitting }: FormikHelpers<any>) => {
+  // 目前只能从业务上解决number的空字符串问题
+  // values.age = undefinedOrNumber(values.age)
   console.log(values);
   // formik似乎无法自动将submitting切换为false状态,因此需要手动调用
   // 而useFormik的声明,可能落后于onSubmit的定义,因此onSubmit的第二个参数中,附带了一些动作函数.
@@ -42,7 +63,7 @@ export default function Home(props: HomeProps) {
     onSubmit,
   });
 
-  const { values, handleSubmit } = formik;
+  const { values, handleSubmit, setFieldValue } = formik;
 
   return (
     <div className={styles.Home}>
@@ -52,6 +73,7 @@ export default function Home(props: HomeProps) {
         <form onSubmit={handleSubmit} noValidate style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
           <FormikInput label="First Name" name="firstName" />
           <FormikInput label="Last Name" name="lastName" />
+          <FormikInput type="number" label="Age" name="age" />
           <FormikInput label="Email Address" name="email" />
           <FormikTextArray label="test" name="test" />
           {/* Formik(issues1476)说想添加一个useFieldArray来获取render中的arrayHelper参数,但最终没有人负责搞出来,仅有人自己封装一些hook */}
@@ -80,7 +102,11 @@ export default function Home(props: HomeProps) {
         </form>
       </FormikProvider>
 
-      <button onClick={() => console.log(formik)}>debug</button>
+      <button onClick={() => console.log(formik.values)}>debug values</button>
+      <button onClick={() => console.log(formik.errors)}>debug errors</button>
+      <button onClick={() => setFieldValue('age', '')}>set space</button>
+      <button onClick={() => setFieldValue('age', null)}>set null</button>
+      <button onClick={() => setFieldValue('age', undefined)}>set undefined</button>
     </div>
   );
 }
